e52f083f89062ab7d8b3d45c59a38947e5e00d26
Lars Bergvall <lars.m.bergvall@gmail.com>
2023-08-30 09:33:10 +0200

    Implemented an AfterCreating hook

---
+++ ModelFactories.Tests/Unit/HooksTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using ModelFactories.Tests.Factories;
+using ModelFactories.Tests.Models;
+
+namespace ModelFactories.Tests;
+
+public class HooksTest
+{
+	[Fact]
+	public void AfterHookIsExecutedWhenCreatingOne()
+	{
+		var post = new PostFactory()
+			.AfterCreate((Post post) =>
+			{
+				post.Title = "This is changed in the after creating hook";
+				return post;
+			})
+			.Create();
+
+		post.Title.Should().Be("This is changed in the after creating hook");
+	}
+
+	[Fact]
+	public void AfterHookIsExecutedForEachItemWhenCreatingMany()
+	{
+		List<Post> posts = new PostFactory()
+			.AfterCreate((Post post) =>
+			{
+				post.Title = "After";
+
+				return post;
+			})
+			.CreateMany(2);
+
+		posts.Should().HaveCount(2);
+		posts.ForEach(post => post.Title.Should().Be("After"));
+	}
+}
