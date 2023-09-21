d37bd6c817dcc9ca88e991297ea9baf869743ec1
Lars Bergvall <lars.m.bergvall@gmail.com>
2023-09-21 11:41:18 +0200

    implemented WithMany

--- ModelFactories/RelatedDefinition.cs
+++ ModelFactories/RelatedDefinition.cs
@@ -4,8 +4,43 @@ public interface IRelatedDefinition
 {
     string PropertyName { get; }
     Delegate Callback { get; }
-
     Delegate CreateFactory { get; }
+}
+
+public class ManyRelatedDefinition<TModel, TFactory> : IRelatedDefinition
+    where TModel : class, new()
+    where TFactory : ModelFactory<TModel>, new()
+{
+    public ManyRelatedDefinition(string propertyName, uint count, Func<TFactory, ModelFactory<TModel>> callback)
+    {
+        PropertyName = propertyName;
+        Count = count;
+        Callback = factory => callback(factory).CreateMany(count);
+    }
+
+    public ManyRelatedDefinition(string propertyName, Func<TFactory, List<TModel>> callback)
+    {
+        PropertyName = propertyName;
+        Callback = callback;
+    }
+
+    public ManyRelatedDefinition(string propertyName, uint count)
+    {
+        PropertyName = propertyName;
+        Count = count;
+        Callback = factory => factory.CreateMany(Count);
+    }
+
+    private uint Count { get; set; }
+    private Func<TFactory, List<TModel>> Callback { get; set; }
+    public string PropertyName { get; }
+    Delegate IRelatedDefinition.Callback => Callback;
+    Delegate IRelatedDefinition.CreateFactory => CreateFactory;
+
+    private TFactory CreateFactory()
+    {
+        return new TFactory();
+    }
 }
 
 public class RelatedDefinition<TModel, TFactory> : IRelatedDefinition where TModel : class, new()
@@ -17,6 +52,7 @@ public class RelatedDefinition<TModel, TFactory> : IRelatedDefinition where TMod
 
         Callback = callback ?? (factory => factory.Create());
     }
+
 
     private Func<TFactory, TModel> Callback { get; set; }
     public string PropertyName { get; private set; }
